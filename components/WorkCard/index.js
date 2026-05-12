dafd5b57e48f661074f396b818cbb5df43e5562d
Ivandaza04 <ivandaza2004@gmail.com>
2026-05-11 21:03:20 -0300

    url

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -20,8 +20,8 @@ const WorkCard = ({ img, name, url, description, title, area, duration, technolo
 
   return (
     <a
-      href={url || "#"}
-      target="_blank"
+      href={url || {url}} 
+      target="_blank" 
       rel="noopener noreferrer"
       className="block overflow-hidden rounded-2xl p-2 laptop:p-4 cursor-pointer group transition-all duration-300 hover:scale-[1.02] hover:shadow-purple-500/30"
       onClick={onClick}
